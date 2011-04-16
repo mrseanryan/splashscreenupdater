dc2175db1ef8787cc3a8ad139c363195500e7050
Sean Ryan <str_ie@yahoo.co.uk>
2011-04-16 20:32:30 +0100

    Fixed issue where bad args would cause uncaught exception.

--- source/SplashScreenUpdater/Program.cs
+++ source/SplashScreenUpdater/Program.cs
@@ -7,33 +7,43 @@ namespace SplashScreenUpdater
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            ArgsProcessor argProc = new ArgsProcessor(args);
-
-            if (!argProc.Validate())
+            try
             {
-                Console.WriteLine(argProc.GetUsage());
-                return;
+                ArgsProcessor argProc = new ArgsProcessor(args);
+
+                if (!argProc.Validate())
+                {
+                    Console.WriteLine(argProc.GetUsage());
+                    return 1;
+                }
+
+                Font font = new Font(argProc.GetArg(ArgsProcessor.ARG_FONT), argProc.GetArgAsInt(ArgsProcessor.ARG_FONT_SIZE), FontStyle.Regular);
+
+                string text, bmpFilePath;
+                bmpFilePath = argProc.GetArg(ArgsProcessor.ARG_BMP_PATH);
+                text = argProc.GetArg(ArgsProcessor.ARG_TEXT);
+                log("Writing text " + text + " on bitmap file at " + bmpFilePath);
+
+                int iRed = argProc.GetArgAsInt(ArgsProcessor.ARG_COLOR_RED);
+                int iGreen = argProc.GetArgAsInt(ArgsProcessor.ARG_COLOR_GREEN);
+                int iBlue = argProc.GetArgAsInt(ArgsProcessor.ARG_COLOR_BLUE);
+                Color color = Color.FromArgb(iRed, iGreen, iBlue);
+
+                BmpTextWriter.WriteTextOnBmp(bmpFilePath, text, font,
+                    argProc.GetArgAsInt(ArgsProcessor.ARG_FONT_SIZE),
+                    new Point(argProc.GetArgAsInt(ArgsProcessor.ARG_POS_X), argProc.GetArgAsInt(ArgsProcessor.ARG_POS_Y)),
+                    color
+                    );
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.StackTrace + "\n" + e.Message);
+                return 2;
             }
 
-            Font font = new Font(argProc.GetArg(ArgsProcessor.ARG_FONT), argProc.GetArgAsInt(ArgsProcessor.ARG_FONT_SIZE), FontStyle.Regular);
-
-            string text, bmpFilePath;
-            bmpFilePath = argProc.GetArg(ArgsProcessor.ARG_BMP_PATH);
-            text = argProc.GetArg(ArgsProcessor.ARG_TEXT);
-            log("Writing text " + text + " on bitmap file at " + bmpFilePath);
-
-            int iRed = argProc.GetArgAsInt(ArgsProcessor.ARG_COLOR_RED);
-            int iGreen = argProc.GetArgAsInt(ArgsProcessor.ARG_COLOR_GREEN);
-            int iBlue = argProc.GetArgAsInt(ArgsProcessor.ARG_COLOR_BLUE);
-            Color color = Color.FromArgb(iRed, iGreen, iBlue);
-
-            BmpTextWriter.WriteTextOnBmp(bmpFilePath, text, font, 
-                argProc.GetArgAsInt(ArgsProcessor.ARG_FONT_SIZE),
-                new Point(argProc.GetArgAsInt(ArgsProcessor.ARG_POS_X), argProc.GetArgAsInt(ArgsProcessor.ARG_POS_Y)),
-                color
-                );
+            return 0;
         }
 
         private static void log(string text)
