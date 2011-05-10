a453ef7869109fe02d574b24b813a2b181ce542e
Sean Ryan <str_ie@yahoo.co.uk>
2011-05-10 13:24:18 +0100

    Updating the test script

--- source/SplashScreenUpdater/test_data/run_release_test.bat
+++ source/SplashScreenUpdater/test_data/run_release_test.bat
@@ -1,7 +1,7 @@
 REM test the Release build on the test_data BMP files
 
-..\bin\Release\SplashScreenUpdater.exe    "Times New Roman" 18  "Setup.bmp"  "CardWorld Acquirer 3.5.0.0"       23 104    80 00 80
+..\bin\Release\SplashScreenUpdater.exe    "Times New Roman" 18  "Setup.bmp"  "My Product 1.0"       23 104    80 00 80
 
-..\bin\Release\SplashScreenUpdater.exe    "Arial Narrow" 14  "Setup.bmp"  "© CR2 2011"       420 280    255 255 255
+..\bin\Release\SplashScreenUpdater.exe    "Arial Narrow" 14  "Setup.bmp"  "© MyCompany 2011"       180 104 255 255 255
 
 PAUSE
