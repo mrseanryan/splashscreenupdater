086136cd140d4b0ddb4a5b4a6093adeee429c95f
Sean Ryan <str_ie@yahoo.co.uk>
2011-05-10 13:20:47 +0100

    Adding test script

---
+++ source/SplashScreenUpdater/test_data/run_release_test.bat
@@ -0,0 +1,7 @@
+REM test the Release build on the test_data BMP files
+
+..\bin\Release\SplashScreenUpdater.exe    "Times New Roman" 18  "Setup.bmp"  "CardWorld Acquirer 3.5.0.0"       23 104    80 00 80
+
+..\bin\Release\SplashScreenUpdater.exe    "Arial Narrow" 14  "Setup.bmp"  "© CR2 2011"       420 280    255 255 255
+
+PAUSE
